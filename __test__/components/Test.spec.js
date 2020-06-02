1660fd5b6bcda25f420b6f86ff963cd7a786e702
Kah Pin <kahpin@KPs-Mac.local>
2020-06-02 11:22:29 +0800

    add testing

---
+++ __test__/components/Test.spec.js
@@ -0,0 +1,10 @@
+import React from "react";
+import { create } from "react-test-renderer";
+import { InputComponent } from '../../src/components/InputComponent/InputComponent';
+
+describe("Input component", () => {
+    test("Matches the snapshot", () => {
+        const input = create(<InputComponent />);
+        expect(input.toJSON()).toMatchSnapshot();
+    });
+});
